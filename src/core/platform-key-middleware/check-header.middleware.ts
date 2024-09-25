381cbcb54b7965dbddc83af80f091c71eae3dca6
Hazem Safwat <HSafwat@moj.gov.sa>
2024-09-26 02:06:49 +0300

    some modification for the x-platform in the header request

--- src/core/platform-key-middleware/check-header.middleware.ts
+++ src/core/platform-key-middleware/check-header.middleware.ts
@@ -8,7 +8,7 @@ import {
 export class CheckHeaderMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: () => void) {
         const xPlatformHeader = req.headers['x-platform'];
-        if (req.url.startsWith('/api/') && xPlatformHeader !== 'yu2ahel') {
+        if (req.url.startsWith('/api/') && xPlatformHeader !== process.env.PLATFORM_NAME ) {
             throw new UnauthorizedException('Invalid X platform Key in the Header');
         }
         next();
