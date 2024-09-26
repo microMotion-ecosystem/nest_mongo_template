f8c5d934b299fcdab579ccaca3086cbe0516972e
Hazem Safwat <HSafwat@moj.gov.sa>
2024-09-26 04:46:41 +0300

    create powershell batch to start new app

--- StartNewApp.ps1
+++ StartNewApp.ps1
@@ -1,8 +1,16 @@
 # Get the name of the parent folder
 $parentFolderName = Split-Path -Leaf (Get-Location)
 
+
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git push -u origin master
+
 # Step 1: Add new remote link in git
 git remote add template https://github.com/microMotion-ecosystem/nest_mongo_template.git
+git merge template/master --no-stat -v --allow-unrelated-histories
+
 
 # Step 2: Search and replace <APP_NAME> with the parent folder name
 Get-ChildItem -Recurse -Include *.ts,*.json, .env, .env.* | ForEach-Object {
